7902eacc4267987d7ad7e306fa4cad678977b4ae
Jesper Orb <github@jesperorb.com>
2025-07-20 09:44:30 +0200

    handle mobile and desktop actions better

--- src/feature/table/index.tsx
+++ src/feature/table/index.tsx
@@ -2,6 +2,7 @@ import { AppShell, Container, Group, Title } from "@mantine/core";
 
 import { useBeerLocations } from "@common/api/queries";
 import { BeerLocationTable } from "@common/components/beer-location-table";
+import InformationActionsMobile from "@common/components/information-actions.mobile";
 import Layout from "@common/components/layout";
 
 const ViewBeerLocationsTable = () => {
@@ -20,6 +21,11 @@ const ViewBeerLocationsTable = () => {
 					/>
 				</Container>
 			</AppShell.Main>
+			<AppShell.Navbar>
+				<Group hiddenFrom="sm" w="100%" px="md" pt="xl">
+					<InformationActionsMobile />
+				</Group>
+			</AppShell.Navbar>
 		</Layout>
 	);
 };
